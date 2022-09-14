bea63a2ea2ae42a91f940bd7b25b3eeb3d6055a3
Wojtek <w.lewandowski@yahoo.com>
2022-09-14 23:26:39 +0200

    ci: install `@manypkg/get-packages`

--- scripts/create-release.js
+++ scripts/create-release.js
@@ -1,1 +1,40 @@
-console.log('GENERATED_TAGS', process.env.GENERATED_TAGS);
+const { createRequire } = __original_require__('module');
+const cwd = process.cwd();
+const _require = createRequire(cwd);
+const { getPackages } = _require('@manypkg/get-packages');
+
+let myError = '';
+console.log(getPackages);
+const { packages, tool } = await getPackages(cwd);
+
+console.log(packages);
+
+const newTagRegex = /New tag:\s+(@[^/]+\/[^@]+|[^/]+)@([^\s]+)/;
+const skipRegex = /Skipping tag \(already exists\):\s+(@[^/]+\/[^@]+|[^/]+)@([^\s]+)/;
+const releasedPackages = [];
+const options = {
+  cwd,
+  listeners: {
+    stdout: (data) => {
+      const match = data.toString().match(newTagRegex);
+      const matchSkip = data.toString().match(skipRegex);
+
+      console.log(matchSkip);
+
+      if (match === null) {
+        return;
+      }
+
+      console.log('match', match);
+
+      releasedPackages.push(match[1]);
+    },
+    stderr: (data) => {
+      myError += data.toString();
+    },
+  },
+};
+
+await exec.exec('yarn', ['changeset', 'tag'], options);
+
+console.log('releasedPackages', releasedPackages);
