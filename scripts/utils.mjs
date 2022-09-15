8d80473351f26bd5f51374bbb84064d58796edf8
Wojtek <w.lewandowski@yahoo.com>
2022-09-15 23:45:42 +0200

    ci: add `createExec`

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -40,7 +40,7 @@ export function getChangelogEntry(changelog, version) {
   for (let i = 0; i < nodes.length; i++) {
     let node = nodes[i];
     if (node.type === "heading") {
-      const stringified = mdastToString(node);
+      const stringified = mdastToString.toString(node);
       const match = stringified.toLowerCase().match(/(major|minor|patch)/);
 
       if (match !== null) {
